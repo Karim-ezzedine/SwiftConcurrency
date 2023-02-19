a600dba06aa93777519f782cbea0a94c4dd3426e
Karim-ezzedine <67189317+Karim-ezzedine@users.noreply.github.com>
2023-02-19 23:01:35 +0200

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Karim Ezzedine on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
